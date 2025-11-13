bf0eebe06a8e6b190386e3771dbf553a255f3300
aninhasalesp <ana@gonzaga.dev>
2025-11-13 20:11:50 -0300

    Add VideoComments command to retrieve and export YouTube video comments

--- youtool/commands/__init__.py
+++ youtool/commands/__init__.py
@@ -3,10 +3,11 @@ from typing import List
 from .base import Command
 from .channel_id import ChannelId
 from .channel_info import ChannelInfo
+from .video_comments import VideoComments
 from .video_info import VideoInfo
 from .video_search import VideoSearch
 
-COMMANDS: List[Command] = [ChannelId, ChannelInfo, VideoInfo, VideoSearch]
+COMMANDS: List[Command] = [ChannelId, ChannelInfo, VideoInfo, VideoSearch, VideoComments]
 
 __all__ = [
     "Command",
@@ -15,4 +16,5 @@ __all__ = [
     "ChannelInfo",
     "VideoInfo",
     "VideoSearch",
+    "VideoComments",
 ]
---
+++ youtool/commands/video_comments.py
@@ -0,0 +1,46 @@
+from typing import List, Self
+
+from youtool import YouTube
+
+from .base import Command
+
+
+class VideoComments(Command):
+    """
+    Get comments from a video ID, generate CSV output
+    """
+
+    name = "video-comments"
+    arguments = [
+        {"name": "--ids", "type": str, "help": "Video ID", "required": True},
+        {"name": "--output-file-path", "type": str, "help": "Output CSV file path"},
+    ]
+
+    COMMENT_COLUMNS: List[str] = ["comment_id", "author_display_name", "text_display", "like_count", "published_at"]
+
+    @classmethod
+    def execute(cls: Self, **kwargs) -> str:
+        """
+        Execute the get-comments command to fetch comments from a YouTube video and save them to a CSV file.
+
+        - a YouTube video ID (`--ids`).
+
+        Args:
+            ids (str): The ID of the YouTube video.
+            output_file_path (str): Path to the output CSV file where comments will be saved.
+            api_key (str): The API key to authenticate with the YouTube Data API.
+
+        Returns:
+            A message indicating the result of the command. If output_file_path is specified,
+            the message will include the path to the generated CSV file.
+            Otherwise, it will return the result as a string.
+        """
+        ids = kwargs.get("ids")
+        output_file_path = kwargs.get("output_file_path")
+        api_key = kwargs.get("api_key")
+
+        youtube = YouTube([api_key], disable_ipv6=True)
+
+        comments = list(youtube.video_comments(ids))
+
+        return cls.data_to_csv(data=comments, output_file_path=output_file_path)
